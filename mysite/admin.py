131cb7ebc4f87a5e52b0d44ec10b825e9bb0e7d4
Tomvictor <vjtomvictor@icloud.com>
2016-07-24 19:42:54 +0530

    problem solved
    Now the user model have additional objects,,,check the admin

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from mysite.models import accounts
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 # Register your models here.
 
-admin.site.register(accounts)
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class Accountsinline(admin.StackedInline):
+    model = accounts
+    can_delete = False
+    verbose_name_plural = 'accounts'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (Accountsinline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
